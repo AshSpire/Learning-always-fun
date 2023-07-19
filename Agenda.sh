8b1afab6130be89d2e76fff0c2f1609d97a9b39c
AshSpire <ashdubo@gmail.com>
2023-07-19 21:52:08 +0100

    First Commit

---
+++ Agenda.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+
+		LEARNIG DEVOPS
+
+
+	TRAINER & MENTOR : NAGESH
+
+
+	1. GIT
+		
+		created Git hub handler " AshSpire "
+		created remote repo " Learning always fun "
+
+		created local repo " DevPractice " using comands mkdir and git init
+
+		created this file using " vi Agenda "
